004d63c5fc1a29b26851f610dabc90c5d980626a
Larisa46472 <Angel46472@gmail.com>
2021-11-22 21:17:34 +0300

    создали вычисления в модальном окне

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
  const btnCart = document.getElementById('cart');
 const modal = document.querySelector('.modal');
 const btnClose = document.querySelector('.btn-close');
+const rows = modal.querySelectorAll('.row');
+const total = modal.querySelector('.modal-sum');
+
+
 //console.log(btnCart);//
 
 //const btnCart = document.getElementsByClassName('button--secondary');
 
 
 
-
+//объявление функций
 
 
 const modalOpen = () => {
@@ -17,6 +21,64 @@ const modalClose = () => {
     modal.classList.add('modal--close')
 }
 
+const getFullPrice =() => {
+    let fullPrice =0;
+    //const rows = modal.querySelectorAll('.row'); 
+    rows.forEach(row => {
+        let newPrice=0;
+        let priceBlock = row.querySelector('.price')
+        let price = +priceBlock.textContent
+        fullPrice+= price;
+    })
+
+    
+    total.textContent=fullPrice;
+  
+}
+
+
+
+//вызов функций
+getFullPrice()
+
 btnCart.addEventListener('click', modalOpen);
 btnClose.addEventListener('click', modalClose);
+
+rows.forEach(row => {
+    let newPrice=0;
+    let priceBlock = row.querySelector('.price')
+    let price = +priceBlock.textContent
+    let countBlock = row.querySelector('.count')
+    let count = countBlock.textContent
+    const btnMinus = row.querySelector('.minus')
+    const btnPlus = row.querySelector('.plus')
+    
+    const getNewPrice = (count, price) =>{
+        newPrice=count * price
+        priceBlock.textContent=newPrice
+        getFullPrice()
+
+    }
+
+    
+   
+  btnMinus.addEventListener('click', ()=> {
+    if(count > 0) {
+    count--
+    countBlock.textContent=count
+    getNewPrice(count, price)
+}
+  })
+  btnPlus.addEventListener('click', ()=> {
+    count++
+    countBlock.textContent=count
+    getNewPrice(count, price)
+})
+   
+   
+    
+})  
+
+
+//логеры
 
