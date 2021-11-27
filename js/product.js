f54cc898553e03010700788c1b6d0a11a1a5c4d7
Larisa46472 <Angel46472@gmail.com>
2021-11-27 14:13:50 +0300

    работает кнопки в ьодальном окне

--- js/product.js
+++ js/product.js
@@ -1,80 +1,61 @@
 const cardsBlock = document.querySelector('#restourant .cards');
 const cards = cardsBlock.querySelectorAll('.card');
-
 const total = modal.querySelector('.modal-sum');
 const rowsBlock = modal.querySelector('.modal-main');
-
 let rows = rowsBlock.querySelectorAll('.row');
-//для сточек в модальном окне
-let nameArr = [] 
+let nameArr = [];
 
-// для подсчета ИТОГО
-const fullPrice = (rows) => {
-    let fullPrice = 0
+const fullPrice = () => {
+    let fullPrice = 0;
     rows.forEach((row, index) => {
-        if (index != 0) { 
-       const price = +row.querySelector('.price').textContent
-       let  count = +row.querySelector('.count').textContent 
-       pricePosition =count * price
+        if (index !== 0) {
+            const price = +row.querySelector('.price').textContent;
+            let count = +row.querySelector('.count').textContent;
+            pricePosition = count * price;
+            fullPrice += pricePosition;
 
-         fullPrice += pricePosition   
-      
         }
-    })
+    });
+    console.log(fullPrice)
     total.textContent = fullPrice
-}
-
-// обновление колличества продукта в строке модального окна
+};
 const getNowRow = () => {
-  rows = rowsBlock.querySelectorAll('.row');
-  rows.forEach((row, index) => {
-      if (index !=0) {
-       const name = row.querySelector('.product-name').textContent
-       const price = row.querySelector('.price').textContent
-      const btnMinus = row.querySelector('.minus');
-       const btnPlus = row.querySelector('.plus');
-        
-        
-        let  count = +row.querySelector('.count').textContent   
-
-      btnMinus.addEventListener('click', () => {
-        if (count > 0) { 
-            count--
-            row.querySelector('.count').textContent = count
-            fullPrice(rows)
-        } 
-        
-       })
-
-        btnPlus.addEventListener('click', () => {
-        count++
-        row.querySelector('.count').textContent = count
-        fullPrice(rows)
-     })
-     } 
-    })
-    fullPrice(rows)
-}
-
-
-//Работаем с карточкой товара в ресторане
-    const getProductModal = (productName, productPrice) =>{
+    rows = rowsBlock.querySelectorAll('.row');
+    rows.forEach((row, index) => {
+        if (index !== 0) {
+            const name = row.querySelector('.product-name').textContent;
+            const price = row.querySelector('.price').textContent;
+            const btnMinus = row.querySelector('.minus');
+            const btnPlus = row.querySelector('.plus');
+            let count = +row.querySelector('.count').textContent;
+            btnMinus.addEventListener('click', () => {
+                if (count > 0) {
+                    count--;
+                    row.querySelector('.count').textContent = count;
+                    fullPrice();
+                }
+            });
+            btnPlus.addEventListener('click', () => {
+                count++;
+                row.querySelector('.count').textContent = count;
+                fullPrice();
+            });
+        }
+    });
+    fullPrice();
+};
+const getProductModal = (productName, productPrice) => {
     const newRow = rows[0].cloneNode(true);
     const productNameBlock = newRow.querySelector('.product-name');
     const productPriceBlock = newRow.querySelector('.price');
-    //const countNewRow = +newRow.querySelector('.count').textContent
     productNameBlock.textContent = productName;
     productPriceBlock.textContent = productPrice;
-
     newRow.classList.remove('not--active');
-
-    //проверяем на наличие названия в массиве
     if (!nameArr.includes(productName)) {
         rowsBlock.append(newRow);
         nameArr.push(productName);
     } else {
         newRow.remove();
-        //увеличиваем счетчик на 1
         rows = rowsBlock.querySelectorAll('.row');
         rows.forEach(row => {
             const name = row.querySelector('.product-name').textContent;
@@ -83,25 +64,20 @@ const getNowRow = () => {
                 counter++;
                 row.querySelector('.count').textContent = counter;
             }
-
         });
     }
-
-    //считаем ИТОГО
-}
-//Работаем с модальным окном
+    getNowRow();
+};
 const infoFromCard = () => {
     cards.forEach(card => {
-        const productNameBlock =card.querySelector('.cart-title');
+        const productNameBlock = card.querySelector('.card-title');
         const productPriceBlock = card.querySelector('.product-price');
-        const productName = productNameBlock.textContent
-        const productPrice = +productPriceBlock.textContent.slice(0, 3)
-        const btnCard = card.querySelector('.button')
-      
-        btnCard.addEventListener('click', () =>{
-            getProductModal(productName, productPrice)
-        })
-    })
-}
-
+        const productName = productPriceBlock.textContent;
+        const productPrice = +productPriceBlock.textContent.slice(0, 3);
+        const btnCard = card.querySelector('.button');
+        btnCard.addEventListener('click', () => {
+            getProductModal(productName, productPrice);
+        });
+    });
+};
 infoFromCard();
